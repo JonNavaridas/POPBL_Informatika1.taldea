1f6982d9d56d4ba95c2ad3da27adf9dd000d7f16
Jon Navaridas <jon.navaridas@alumni.mondragon.edu>
2019-12-28 11:50:53 +0100

    Miau 3: El retorno del Jedi

--- sdlExamplesVcWS/01simpleStaticDemo/01simpleStaticDemoMain.c
+++ sdlExamplesVcWS/01simpleStaticDemo/01simpleStaticDemoMain.c
@@ -7,25 +7,40 @@
 #include <windows.h>
 #include <math.h>
 
+//Base del juego
 int hasieratu(void);
+void backgroundCreate(void);
+//Creación de las torres
+int towerCreate(int posX, int posY, int ebentu);
+int buildArcherTower(int posX, int posY);
+int buildMageTower(int posX, int posY);
+int buildBomberTower(int posX, int posY);
+int buildMiningTower(int posX, int posY);
+//Targeting de las torres
+int towerTarget(int x, int y, int positionX, int positionY, int radius, int time);
+POSIZIOA diferentziaPos(POSIZIOA posTower, POSIZIOA posEnemy);
+
 
 int main(int argc, char * str[]) 
 {
 	int ebentu = 0, irten = 0, mugitu = 0, tmp = 0;
-	int tower = 0, time = 0, towerRadius;
-	POSIZIOA pos;
+	int tower = 0, time = 0, towerRadius = 0, towerRadiusTmp = 0;
+	POSIZIOA enemyPos,towerPos;
 	 
 	hasieratu();
-	pos.x = 10;
-	pos.y = SCREEN_HEIGHT * 2 / 3 - 50;
+	enemyPos.x = 10;
+	enemyPos.y = SCREEN_HEIGHT * 2 / 3;
+	towerPos.x = SCREEN_WIDTH / 2;
+	towerPos.y = SCREEN_HEIGHT * 2 / 3 - 50;
 	arkatzKoloreaEzarri(255, 255, 255);
 	
 	while(irten == 0)
 	{
-		Sleep(1);
+		Sleep(5);
+		time++;
 		ebentu = ebentuaJasoGertatuBada();
 		tower = ebentu;
-		if (tower != TECLA_1 && tower != TECLA_2 && tower != TECLA_3 && tower != TECLA_4) { tower = tmp; }
+		if (tower != TECLA_1 && tower != TECLA_2 && tower != TECLA_3 && tower != TECLA_4) { tower = tmp; towerRadiusTmp = 0; }
 		tmp = tower;
 		
 		//Hasieratu eta amaitu
@@ -33,23 +48,25 @@ int main(int argc, char * str[])
 		if (ebentu == TECLA_RETURN && mugitu == 0) mugitu = 1;
 		
 		//Dorre motak
-		towerRadius = towerCreate(SCREEN_WIDTH / 2, SCREEN_HEIGHT * 2 / 3 - 50, tower, time);
+		towerRadius = towerRadiusTmp;
+		if (towerRadius == 0) { towerRadius = towerCreate(towerPos.x, towerPos.y, tower); }
+		towerRadiusTmp = towerRadius;
 		backgroundCreate();
 		
 		//Bolaren mugimendua
 		arkatzKoloreaEzarri(255, 255, 255);
 		if (mugitu == 1) {
-			zirkuluaMarraztu(pos.x, pos.y + 50, 10);
-			pos.x += 1;
-			if (pos.x == SCREEN_WIDTH) mugitu = 2;
+			zirkuluaMarraztu(enemyPos.x, enemyPos.y, 10);
+			enemyPos.x += 1;
+			if (enemyPos.x == SCREEN_WIDTH) mugitu = 2;
 		}
 		if (mugitu == 2) {
-			zirkuluaMarraztu(pos.x, pos.y + 50, 10);
-			pos.x -= 1;
-			if (pos.x == 0) mugitu = 1;
+			zirkuluaMarraztu(enemyPos.x, enemyPos.y, 10);
+			enemyPos.x -= 1;
+			if (enemyPos.x == 0) mugitu = 1;
 		}
 
-		towerTarget(pos.x, pos.y, SCREEN_WIDTH / 2, SCREEN_HEIGHT * 2 / 3 - 100, towerRadius);
+		if (tower != 0) towerTarget(enemyPos.x, enemyPos.y, towerPos.x, towerPos.y, towerRadius, time);
 		pantailaBerriztu();
 		pantailaGarbitu();
 	}
@@ -68,41 +85,39 @@ int hasieratu(void)
 	return 0;
 }
 
-int backgroundCreate(void)
+void backgroundCreate(void)
 {
 	textuaIdatzi(10, 20, "Pruebas");
 	arkatzKoloreaEzarri(255, 255, 255);
 	zirkuluaMarraztu(SCREEN_WIDTH / 2, SCREEN_HEIGHT * 2 / 3 - 50, 5);
 	zuzenaMarraztu(0, SCREEN_HEIGHT * 2 / 3, SCREEN_WIDTH, SCREEN_HEIGHT * 2 / 3);
 	pantailaBerriztu();
-
-	return 0;
 }
 
-int towerCreate(int posX, int posY, int ebentu, int time)
+int towerCreate(int posX, int posY, int ebentu)
 {
-	int tower = 0;
+	int tower = 0, radius = 0;
 
 	if (ebentu == TECLA_1 || tower == 1) {
-		buildArcherTower(posX, posY, time);
+		radius = buildArcherTower(posX, posY);
 		tower = 1;
 	}
 	if (ebentu == TECLA_2 || tower == 2) {
-		buildMageTower(posX, posY, time);
+		radius = buildMageTower(posX, posY);
 		tower = 2;
 	}
 	if (ebentu == TECLA_3 || tower == 3) {
-		buildBomberTower(posX, posY, time);
+		radius = buildBomberTower(posX, posY);
 		tower = 3;
 	}
 	if (ebentu == TECLA_4 || tower == 4) {
-		buildMiningTower(posX, posY, time);
+		radius = buildMiningTower(posX, posY);
 		tower = 4;
 	}
-	return 0;
+	return radius;
 }
 
-int buildArcherTower(int posX, int posY, int time)
+int buildArcherTower(int posX, int posY)
 {
 	arkatzKoloreaEzarri(0, 50, 50);
 	zirkuluaMarraztu(posX, posY, 105);
@@ -110,7 +125,7 @@ int buildArcherTower(int posX, int posY, int time)
 	return 105;
 }
 
-int buildMageTower(int posX, int posY, int time)
+int buildMageTower(int posX, int posY)
 {
 	arkatzKoloreaEzarri(100, 0, 100);
 	zirkuluaMarraztu(posX, posY, 75);
@@ -118,7 +133,7 @@ int buildMageTower(int posX, int posY, int time)
 	return 75;
 }
 
-int buildBomberTower(int posX, int posY, int time)
+int buildBomberTower(int posX, int posY)
 {
 	arkatzKoloreaEzarri(150, 150, 0);
 	zirkuluaMarraztu(posX, posY, 150);
@@ -126,7 +141,7 @@ int buildBomberTower(int posX, int posY, int time)
 	return 150;
 }
 
-int buildMiningTower(int posX, int posY, int time)
+int buildMiningTower(int posX, int posY)
 {
 	arkatzKoloreaEzarri(200, 200, 200);
 	zirkuluaMarraztu(posX, posY, 20);
@@ -134,32 +149,34 @@ int buildMiningTower(int posX, int posY, int time)
 	return 20;
 }
 
-int towerTarget(int x, int y, int positionX, int positionY, int radius)
+int towerTarget(int x, int y, int positionX, int positionY, int radius, int time)
 {
-	POSIZIOA div, tower, enemy;
+	POSIZIOA dif, tower, enemy, bullet;
 	tower.x = positionX;
 	tower.y = positionY;
 	enemy.x = x;
 	enemy.y = y;
+	bullet.x = tower.x;
+	bullet.y = tower.y;
 
-	//div = projectileTrajectory(tower, enemy);
-	if (x <= positionX + radius && x >= positionX - radius && y <= positionY + radius && y >= positionY - radius)
+	if (enemy.x <= tower.x + radius && enemy.x >= tower.x - radius && enemy.y <= tower.y + radius && enemy.y >= tower.y - radius)
 	{
 		zuzenaMarraztu(positionX, positionY, x, y);
-		if ((positionX + div.x) != x && (positionY + div.y) != y) {
-			zirkuluaMarraztu(positionX + div.x, positionY + div.y, 5);
-		}
+		arkatzKoloreaEzarri(0, 0, 0);
+		dif = diferentziaPos(tower, enemy);
+		bullet.x += dif.x;
+		bullet.y += dif.y;
+		zirkuluaMarraztu(bullet.x, bullet.y, 2);
 	}
 	pantailaBerriztu();
 
 	return 0;
 }
 
-/*POSIZIOA projectileTrajectory(POSIZIOA posTower, POSIZIOA posEnemy)
+POSIZIOA diferentziaPos(POSIZIOA posTower, POSIZIOA posEnemy)
 {
 	POSIZIOA diferentzia;
-	diferentzia.x = 1;
-	diferentzia.y = 1;
-
+	diferentzia.x = posTower.x - posEnemy.x;
+	diferentzia.y = posTower.y - posEnemy.y;
 	return diferentzia;
-}*/
+}
